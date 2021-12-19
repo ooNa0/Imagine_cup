a6e87244208b4d54b232725d907e13ac1b06b936
wook <gus5427@naver.com>
2021-12-19 14:09:52 +0900

    feat connect bottom navigation to fragments

--- Frontend/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Frontend/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.example.myapplication.fragment.HomeFragment;
+import com.example.myapplication.fragment.MissionFragment;
+import com.example.myapplication.fragment.MyPageFragment;
+import com.example.myapplication.fragment.RecordFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout home_layout;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        SettingListener();
+        bottomNavigationView.setSelectedItemId(R.id.tab_home);
+
+    }
+
+    private void init(){
+        home_layout = findViewById(R.id.home_layout);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+    }
+
+    private void SettingListener(){
+        bottomNavigationView.setOnNavigationItemSelectedListener(new TabSelectedListener());
+    }
+
+    class TabSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.tab_home:
+                    changeFragment(new HomeFragment());
+                    return true;
+
+                case R.id.tab_records:
+                    changeFragment(new RecordFragment());
+                    return true;
+
+                case R.id.tab_mission:
+                    changeFragment(new MissionFragment());
+                    return true;
+
+                case R.id.tab_mypage:
+                    changeFragment(new MyPageFragment());
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    private void changeFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.home_layout, fragment).commit();
     }
 }
