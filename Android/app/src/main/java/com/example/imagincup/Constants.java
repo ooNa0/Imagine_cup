022afd85f731d6d0359f43a40f5ca6b4ecf069dc
na0 <koonayoung51@naver.com>
2022-01-07 00:28:38 +0900

    [ADD] Constants class

---
+++ Android/app/src/main/java/com/example/imagincup/Constants.java
@@ -0,0 +1,6 @@
+package com.example.imagincup;
+
+public final class Constants {
+    public static final String	JDBC_DRIVER	= "net.sourceforge.jtds.jdbc.Driver";
+    public static final String	DATABASE_CONNECTION_URL	= "jdbc:jtds:sqlserver://imagine-cup-server.database.windows.net:1433;DatabaseName=epqp;user=na0@imagine-cup-server;password=a12345678*;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;ssl=request";
+}
