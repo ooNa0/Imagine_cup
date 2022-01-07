7747f2dd440ae3a6c4e42f8de117ef3890e5ee20
wook <gus5427@naver.com>
2022-01-07 13:33:08 +0900

    Design : add UI pedometer activity

--- Android/app/src/main/java/com/example/imagincup/MissionActivity.java
+++ Android/app/src/main/java/com/example/imagincup/MissionActivity.java
@@ -43,9 +43,9 @@ public class MissionActivity extends AppCompatActivity {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), Pedometer.class);
+                Intent intent = new Intent(getApplicationContext(), Pedometer.class);
 //                Intent intent = new Intent(getApplicationContext(),Camera.class);
-                Intent intent = new Intent(getApplicationContext(), Record.class);
+//                Intent intent = new Intent(getApplicationContext(), Record.class);
                 startActivity(intent);
             }
         });
