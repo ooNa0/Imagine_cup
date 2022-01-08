1885d12b48add2aca4f1dba954db6ae82e0d6f72
na0 <koonayoung51@naver.com>
2022-01-09 00:42:56 +0900

    [ADD] day card

--- Android/app/src/main/java/com/example/imagincup/fragment/DayRecordFragment.java
+++ Android/app/src/main/java/com/example/imagincup/fragment/DayRecordFragment.java
@@ -8,25 +8,47 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.imagincup.R;
+import com.example.imagincup.back.DTORecord;
+//import com.example.imagincup.back.RecycleViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DayRecordFragment extends Fragment{
 
+    List<DTORecord> listRecord;
+
+    public DayRecordFragment() { }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_record_day, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        listRecord = new ArrayList<>();
+        //listRecord.add(new DTORecord());
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_record_day, container, false);
+
+        //RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview_id);
+        //RecycleViewAdapter recyclerViewAdapter = new RecycleViewAdapter(getContext(),listRecord);
+//        RecycleViewAdapter recyclerViewAdapter = new RecycleViewAdapter(getApplicationContext(),listRecord);
+//        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+//        recyclerView.setLayoutManager(gridLayoutManager);
+//        recyclerView.setAdapter(recyclerViewAdapter);
+
+        return view;
+    }
+
 
     @Override
     public void onDetach() {
         super.onDetach();
     }
 
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void messageFromChildFragment(Uri uri);
-    }
 }
