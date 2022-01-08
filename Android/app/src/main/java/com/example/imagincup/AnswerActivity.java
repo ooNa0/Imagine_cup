a8e1b7fd0a4f8994daa39204ab2280ab15b18463
na0 <koonayoung51@naver.com>
2022-01-09 00:34:55 +0900

    [ADD] AnswerActivity

---
+++ Android/app/src/main/java/com/example/imagincup/AnswerActivity.java
@@ -0,0 +1,15 @@
+package com.example.imagincup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_answer);
+    }
+
+}
