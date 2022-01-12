52278a74159cece2a666768febd04564dbb52499
na0 <koonayoung51@naver.com>
2022-01-13 02:08:48 +0900

    [ADD] Recycleview adapter (데베는 나중에!)

--- Android/app/src/main/java/com/example/imagincup/back/RecycleViewAdapter.java
+++ Android/app/src/main/java/com/example/imagincup/back/RecycleViewAdapter.java
@@ -1,12 +1,74 @@
 package com.example.imagincup.back;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
+import com.example.imagincup.R;
 
-//public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.MyViewHolder>{
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.CustomViewHolder> {
+
+    private CardView cardView;
+    private Context context;
+    private List<Integer> items;
+    private int i;
+
+    public RecycleViewAdapter(Context context, List<Integer> items, int i) {
+        this.context = context;
+        this.items = items;
+        this.i = i;
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_card_day_record, parent, false);
+
+        cardView = view.findViewById(R.id.card_view);
+
+        CustomViewHolder viewHolder = new CustomViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onBindViewHolder(CustomViewHolder holder, int position) {
+        final Integer item = items.get(position);
+        holder.title.setText(item + "");
+        holder.content.setText(item + "content");
+        if(item == 3){
+            cardView.setCardBackgroundColor(Color.parseColor("#CC82DBD7"));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.items.size();
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView content;
+
+        public CustomViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.card_item_detail);
+            content = itemView.findViewById(R.id.card_item_emotion);
+        }
+    }
+
+}
+
 
 //    private final ArrayList<DataModel> dataModelArrayList;
 //    private Context context;
@@ -23,7 +85,7 @@ import java.util.ArrayList;
 //        // 어댑터에서 액티비티 액션을 가져올 때 context가 필요한데 어댑터에는 context가 없다.
 //        // 선택한 액티비티에 대한 context를 가져올 때 필요하다.
 //    }
-//
+
 //    // 뷰홀더
 //    // 이 부분에서 super를 통해 상속을 받았다.
 //    // 이 RecyclerView 에 뷰 holder 에서 상속을 받아서 거기에 아이템 값을 찾아와야 한다.
