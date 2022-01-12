704c8b66ea45b517cf7975c57189aa3a6ba4ed43
na0 <koonayoung51@naver.com>
2022-01-13 02:07:52 +0900

    [ADD] 데일리 결과 화면 (MPAndroid pieChart 마스터..)

--- Android/app/src/main/java/com/example/imagincup/AnswerActivity.java
+++ Android/app/src/main/java/com/example/imagincup/AnswerActivity.java
@@ -1,15 +1,104 @@
 package com.example.imagincup;
 
+import static java.security.AccessController.getContext;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.github.mikephil.charting.animation.ChartAnimator;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.interfaces.datasets.IPieDataSet;
+import com.github.mikephil.charting.renderer.PieChartRenderer;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.Utils;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.util.ArrayList;
 
 public class AnswerActivity extends AppCompatActivity {
+
+    private PieChart pieChart;
+    private PieChart pieEmotionChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_day_answer);
-    }
+        setContentView(R.layout.activity_day_result);
 
+        pieChart = (PieChart)findViewById(R.id.daily_emotions_chart);
+        pieEmotionChart = (PieChart)findViewById(R.id.daily_mission_achievement_chart);
+
+        // data set
+
+        ArrayList<PieEntry> yValues = new ArrayList<PieEntry>();
+        yValues.add(new PieEntry(90,""));
+        yValues.add(new PieEntry(10,""));
+
+        PieDataSet dataSet = new PieDataSet(yValues,"emotions");
+        //ataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        //dataSet.setColor(getResources().getColor(R.color.black));
+        int[] colors = {Color.rgb(153, 193, 12), Color.rgb(179, 130, 76)};
+
+        //dataSet.setGradientColor(Color.parseColor("88BEB7"), Color.parseColor("26746C"));
+
+        dataSet.setColors(getResources().getColor(R.color.mission_button), getResources().getColor(R.color.white_gray));
+
+        PieData data = new PieData(dataSet);
+
+        // 퍼센트 지우기
+        data.setDrawValues(false);
+
+        //////////// design
+
+        // 안에 구멍
+        pieChart.setDrawHoleEnabled(true);
+
+        // 안에 구멍 투명하게
+        pieEmotionChart.setHoleColor(getColor(R.color.transparent));
+        pieChart.setHoleColor(getColor(R.color.transparent));
+
+        // 하단 x-Values List 안보이게
+        pieEmotionChart.getLegend().setEnabled(false);
+        pieChart.getLegend().setEnabled(false);
+
+        // 설명 안보이게
+        pieEmotionChart.getDescription().setEnabled(false);
+        pieChart.getDescription().setEnabled(false);
+
+        // 터치 지켜
+        pieEmotionChart.setTouchEnabled(false);
+        //pieChart.setTouchEnabled(false);
+
+        // 끝이 동그랗게?
+        //pieEmotionChart.setDrawRoundedSlices(true);
+        pieChart.setDrawRoundedSlices(true);
+
+        // 구멍 크기
+        pieEmotionChart.setHoleRadius(93);
+        pieChart.setHoleRadius(93);
+
+        pieEmotionChart.setDrawEntryLabels(false);
+
+        //애니메이션
+        pieEmotionChart.animateY(1000, Easing.EaseInOutCubic);
+        pieChart.animateY(1000, Easing.EaseInOutCubic);
+
+
+        pieEmotionChart.setData(data);
+        pieChart.setData(data);
+    }
 }
