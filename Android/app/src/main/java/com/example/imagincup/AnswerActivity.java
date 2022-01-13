79a7aa47dda9c7135aec9edc44f2b63352c9d97b
na0 <koonayoung51@naver.com>
2022-01-13 21:49:04 +0900

    [ADDING] 질문 답변 연결 및 데베 (하는 중)

--- Android/app/src/main/java/com/example/imagincup/AnswerActivity.java
+++ Android/app/src/main/java/com/example/imagincup/AnswerActivity.java
@@ -1,46 +1,75 @@
 package com.example.imagincup;
 
-import static java.security.AccessController.getContext;
-
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Path;
-import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import com.github.mikephil.charting.animation.ChartAnimator;
+import com.example.imagincup.back.EmotionAsyncTask;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.interfaces.datasets.IPieDataSet;
-import com.github.mikephil.charting.renderer.PieChartRenderer;
-import com.github.mikephil.charting.utils.ColorTemplate;
-import com.github.mikephil.charting.utils.MPPointF;
-import com.github.mikephil.charting.utils.Utils;
-import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
-public class AnswerActivity extends AppCompatActivity {
+public class AnswerActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // 텍스트
+    private TextView dayTextView;
+    private TextView questionTextView;
+    private TextView answerTextView;
+    private EditText answerEditText;
+    private Button saveButton;
+    private LinearLayout resultLinearLayout;
+
+    // 차트
     private PieChart pieChart;
     private PieChart pieEmotionChart;
+
+    // 체크
+    private TextView checkMissonTextView;
+    private TextView emotionIconTextView;
+    private TextView emtionParcentTextView;
+
+    private Button goMissionButton;
+
+    private String answer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_result);
 
+        dayTextView = findViewById(R.id.question_day);
+        questionTextView = findViewById(R.id.question_);
+        answerTextView = findViewById(R.id.question_answer);
+        answerEditText = findViewById(R.id.question_answer_edit);
+        saveButton = findViewById(R.id.save_answer);
+        resultLinearLayout = findViewById(R.id.result_layout);
+
         pieChart = (PieChart)findViewById(R.id.daily_emotions_chart);
         pieEmotionChart = (PieChart)findViewById(R.id.daily_mission_achievement_chart);
+
+        checkMissonTextView = findViewById(R.id.check_misson);
+        emotionIconTextView = findViewById(R.id.emotion_icon);
+        emtionParcentTextView = findViewById(R.id.emotion_parcent);
+
+        goMissionButton = findViewById(R.id.go_misson_button);
+
+        saveButton.setOnClickListener(this);
+        goMissionButton.setOnClickListener(this);
 
         // data set
 
@@ -97,8 +126,57 @@ public class AnswerActivity extends AppCompatActivity {
         pieEmotionChart.animateY(1000, Easing.EaseInOutCubic);
         pieChart.animateY(1000, Easing.EaseInOutCubic);
 
-
         pieEmotionChart.setData(data);
         pieChart.setData(data);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.save_answer:
+            {
+                //new AnalyzeSentiment();
+
+                //answer = answerEditText.getText();
+
+                JSONObject data = null;
+                try {
+                    data = new EmotionAsyncTask().execute().get();
+
+                    Log.d("a", data.getString("sentiment"));
+                    JSONObject parcent = data.getJSONObject("confidenceScores");
+                    Log.d("double", String.valueOf(parcent.getDouble(data.getString("sentiment"))));
+                    if(data.getString("sentiment").equals("positive")){
+                        emotionIconTextView.setText("😀");
+                    }
+                    else if(data.getString("sentiment").equals("negative")){
+                        emotionIconTextView.setText("😢");
+                    }
+                    else{
+                        emotionIconTextView.setText("😶");
+                    }
+                    emtionParcentTextView.setText(String.valueOf(parcent.getDouble(data.getString("sentiment"))*100) + '%');
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            }
+            case R.id.go_misson_button:
+            {
+               break;
+            }
+        }
+    }
+
+    public void SwitchVisibility(boolean isExistText){
+        if(isExistText){
+
+        }
+        else{
+
+        }
     }
 }
---
+++ Android/app/src/main/java/com/example/imagincup/back/EmotionAsyncTask.java
@@ -0,0 +1,120 @@
+package com.example.imagincup.back;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.android.volley.toolbox.HttpResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class EmotionAsyncTask extends AsyncTask<String, Void, JSONObject> {
+
+    private JSONObject responseJson = null;
+
+
+    //ProgressDialog asyncDialog = new ProgressDialog(ProgressDialogActivity.this);
+
+    public EmotionAsyncTask() {
+        super();
+        //this.mContext = mContext;
+    }
+
+    //ProgressDialog progressDialog;
+    public Context mContext;
+
+    // @Override protected Long doInBackground(URL... urls) { // 전달된 URL 사용 작업 return total; }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        // doInBackground 에서 받아온 total 값 사용 장소
+
+        //progressDialog = ProgressDialog.show(mContext, mContext.getString(R.string.message_progress), null, true, true);
+    }
+
+    @Override
+    protected JSONObject doInBackground(String... params) {
+
+        String KEY = "e3fe5d90c4de453ea23e2af93e90cea5";
+        String ENDPOINT = "https://text-emotion.cognitiveservices.azure.com/"; // 따로 빼주기
+        JSONObject data = null;
+
+        try {
+            // 2. HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송
+            URL url = new URL(ENDPOINT + "/text/analytics/v3.0/sentiment"); // 주소가 저장된 변수
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            httpURLConnection.setRequestProperty("Ocp-Apim-Subscription-Key", KEY);
+
+            httpURLConnection.setRequestMethod("POST"); //요청 방식을 POST
+            httpURLConnection.setRequestProperty("Content-Type", "application/json");
+
+            JSONObject jsonObject = new JSONObject();
+            JSONArray jsonArray = new JSONArray();
+            JSONObject json = new JSONObject();
+            json.put("id", "1");
+            json.put("text", "TI wonder what my head will look like now, happy and hard.");
+            jsonArray.put(json);
+            jsonObject.put("documents", jsonArray);
+
+            // Request Body에 데이터를 담기위한 OutputStream 객체 생성
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            outputStream.write(jsonObject.toString().getBytes());
+            Log.d("a", json.toString());
+            outputStream.flush();
+            //outputStream.close();
+
+            // 응답을 읽rl
+            int responseStatusCode = httpURLConnection.getResponseCode();
+            Log.d("TAG", "POST response code - " + responseStatusCode);
+
+            InputStream inputStream;
+            if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+                // 정상적인 응답 데이터
+                inputStream = httpURLConnection.getInputStream();
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder outResult = new StringBuilder();
+                String inputLine = null;
+
+                while ((inputLine = bufferedReader.readLine()) != null) {
+                    outResult.append(inputLine);
+                }
+
+                bufferedReader.close();
+
+                responseJson = new JSONObject(outResult.toString());
+                JSONArray emotions = responseJson.getJSONArray("documents");
+                data = emotions.getJSONObject(0);
+
+                Log.d("a", data.getString("sentiment"));
+                JSONObject parcent = data.getJSONObject("confidenceScores");
+                Log.d("double", String.valueOf(parcent.getDouble(data.getString("sentiment"))));
+
+                Log.d("TAG", "POST response - " + outResult.toString());
+
+            } else {
+                // 에러 발생
+                inputStream = httpURLConnection.getErrorStream();
+            }
+
+
+        } catch (Exception e) {
+            Log.d("TAG", "InsertData: Error ", e);
+        }
+        return data;
+    }
+}
