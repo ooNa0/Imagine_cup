93f9733784c1f56ce7d7ff95e552a8108a617251
wook <gus5427@naver.com>
2022-01-19 00:35:12 +0900

    Feat : 유튜브 랜덤영상 재생

--- Android/app/src/main/java/com/example/imagincup/activity/mission/Music.java
+++ Android/app/src/main/java/com/example/imagincup/activity/mission/Music.java
@@ -1,49 +1,67 @@
 package com.example.imagincup.activity.mission;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.imagincup.R;
+import com.example.imagincup.model.MissionState;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.util.Random;
+
 
 public class Music extends YouTubeBaseActivity {
 
     //객체 선언
     private static String API_KEY = "AIzaSyC6FF8AVtTVOo1P2SaLMUM79zIGH5pvsJg";
     private static String VIDEO_ID = "Gqfk5sr9fpw";
+    private String[] VIDEO_IDs = new String[]{"Gqfk5sr9fpw","MZUR479P2Cw","BR4WG1XlLZw"};
     private static String TAG = "YOUTUBE";
+    private String videoId;
+    private Random random;
 
     YouTubePlayerView youTubePlayerView;
     YouTubePlayer player;
     Button playButton;
-
+    Button clearButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
         initPlayer();
+        random = new Random();
+        videoId = VIDEO_IDs[random.nextInt(3)];
         playButton = findViewById(R.id.youtubeBtn);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                playVideo();
+                playVideo(videoId);
             }
         });
         youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youTubePlayerView);
+        clearButton = findViewById(R.id.music_button_clear);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MissionState.getInstance().setDone(true);
+                Intent intent = new Intent();
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
 
     }
 
-    private void playVideo(){
+    private void playVideo(String VIDEO_ID){
         if(player != null){
             if(player.isPlaying()){
                 player.pause();
