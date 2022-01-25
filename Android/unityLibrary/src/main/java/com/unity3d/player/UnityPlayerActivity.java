56c29d1c0e0741e5a4df67fdd09934a15341db7b
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2022-01-25 18:48:41 +0900

    [add]change unityLibrary

--- Android/unityLibrary/src/main/java/com/unity3d/player/UnityPlayerActivity.java
+++ Android/unityLibrary/src/main/java/com/unity3d/player/UnityPlayerActivity.java
@@ -13,10 +13,6 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.os.Process;
 
-import com.unity3d.player.IUnityPlayerLifecycleEvents;
-import com.unity3d.player.MultiWindowSupport;
-import com.unity3d.player.UnityPlayer;
-
 public class UnityPlayerActivity extends Activity implements IUnityPlayerLifecycleEvents
 {
     protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
